f1574433e6d783c4d6de539da9b136bb0db6cd19
Antonioo27 <129292941+Antonioo27@users.noreply.github.com>
2023-05-31 16:00:42 +0200

    Controller per la pagina iniziale, con metodo relativo al login dell amministratore

--- src/main/java/com/example/provadefinitiva/PaginaInizialeController.java
+++ src/main/java/com/example/provadefinitiva/PaginaInizialeController.java
@@ -1,4 +1,38 @@
 package com.example.provadefinitiva;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+
+
+
+import java.io.IOException;
+
 public class PaginaInizialeController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void loginAdministrator(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        Image icon = new Image("C:\\Users\\HP\\ProvaDefinitiva\\src\\logo.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Pagina Iniziale");
+        stage.show();
+    }
+
+    public void loginPlayer(ActionEvent event) {
+    }
+
 }
+
