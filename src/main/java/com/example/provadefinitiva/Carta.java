ca9ceba6b82f344d52226f11ce41bb9ff9a946b5
Nick-Monster03 <nicola.tinari@studio.unibo.it>
2023-08-28 18:38:58 +0200

    secondo commento dalla bash pero

--- src/main/java/com/example/provadefinitiva/Carta.java
+++ src/main/java/com/example/provadefinitiva/Carta.java
@@ -8,7 +8,7 @@ public class Carta {
         this.s = s;
         this.valore = valore;
     }
-//ciao sono Nicola e sto facendo delle prove con git
+//ciao sono Nicola e sto facendo delle prove con git2
     public String toString(){
         return " [" + this.valore + " di " +
                 this.s + "] ";
